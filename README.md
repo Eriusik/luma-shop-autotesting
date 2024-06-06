bd18b3f935177e1a7df6d0adc8f964dcadafa02f
Eriusik <erika.od.ua@gmail.com>
2024-06-06 14:43:11 +0300

    Update test names and readme

--- README.md
+++ README.md
@@ -1,9 +1,56 @@
 # luma-shop-autotesting
+This a project for launch automated tests for product page `https://magento-2.showcase-wallee.com/olivia-1-4-zip-light-jacket.html#size=166&color=49`
 
-## How to lunch autotests
-Follow the below commands:
+## How to launch autotests
+Clone, install and run the project using commands:
 
 ```
+git clone https://github.com/Eriusik/luma-shop-autotesting.git
 npm install
 npm run cypress:run
+```
+
+## Which tests are covered
+1. Check product options on the product page
+Select different sizes and different colors: product should be added to the cart in all cases
+Negative cases: 
+Product option is not selected - An error is displayed: This is a required field.
+
+2. Add to the cart different quantity of the product
+Type min quantity,  max quantity and middle (Ex. 1, 10, 100) - products should be added to the cart
+Negative cases:
+quantity is 0  - An error is displayed: 'Please enter a quantity greater than 0.'
+quantity is string  - An error is displayed: 'Please enter a valid number in this field.'
+quantity is decimal  - An error is displayed: 'You cannot use decimal quantity for this product.'
+
+3. Add product to the cart several times
+Add product to the cart several times - products should be added to the cart, quantity is increased
+
+## Test Report:
+```
+  Add different size and different count of product to the cart from the product page
+    ✓ Test 3 - add to the cart several times (8592ms)
+    Test 1 - Check product options on the product page
+      ✓ select size XS and add to the cart (5071ms)
+      ✓ select size S and add to the cart (2091ms)
+      ✓ select size M and add to the cart (4763ms)
+      ✓ select size L and add to the cart (4457ms)
+      ✓ select size XL and add to the cart (4220ms)
+      ✓ Test 1 - select color Black and add to the cart (4736ms)
+      ✓ Test 1 - select color Blue and add to the cart (4048ms)
+      ✓ Test 1 - select color Purple and add to the cart (4338ms)
+      ✓ Negative case - size is not selected (3675ms)
+      ✓ Negative case - color is not selected (3636ms)
+    Test 2 - Add to the cart different quantity of the product
+      ✓ Test 2 - add to the cart 1 items (4573ms)
+      ✓ Test 2 - add to the cart 10 items (4710ms)
+      ✓ Test 2 - add to the cart 100 items (4788ms)
+      ✓ Negative case - type decimal quantity (8367ms)
+      ✓ Negative case - 0 quantity (4212ms)
+      ✓ Negative case - type string instead quantity (5125ms)
+      ✓ Negative case - type negative digit instead quantity (4922ms)
+      ✓ Negative case - type negative digit instead quantity (8817ms)
+
+
+  19 passing (2m)
 ```
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,45 +2,99 @@ const sizes = ['XS', 'S', 'M', 'L', 'XL']
 const colors = ['Black', 'Blue', 'Purple']
 const quantityTypes = ['1', '10', '100']
 
-describe('add different size and different count of product to the cart from the product page', () => {
+describe('Add different size and different count of product to the cart from the product page', () => {
   beforeEach('open the product page', () => {
     cy.visit('https://magento-2.showcase-wallee.com/olivia-1-4-zip-light-jacket.html')
     cy.get('h1.page-title').contains('Olivia 1/4 Zip Light Jacket')
     cy.get('[data-option-label="Black"]', { timeout: 20000 }).should('be.visible') // waiting for loading options
   })
-  for (const size of sizes) {
-    it(`select size ${size} and add to the cart`, () => {
-      cy.get(`[data-option-tooltip-value="${size}"]`).click()
+  describe('Test 1 - Check product options on the product page', () => {
+    for (const size of sizes) {
+      it(`select size ${size} and add to the cart`, () => {
+        cy.get(`[data-option-tooltip-value="${size}"]`).click()
+        cy.get('[data-option-label="Black"]').click()
+        cy.get('[id="product-addtocart-button"]').click()
+        cy.get('.page.messages').should('be.visible')
+        cy.get('.page.messages').contains('You added Olivia 1/4 Zip Light Jacket to your shopping cart.')
+        cy.get('.counter-number').contains('1')
+      })
+    }
+    for (const color of colors) {
+      it(`Test 1 - select color ${color} and add to the cart`, () => {
+        cy.get(`[data-option-tooltip-value="S"]`).click()
+        cy.get(`[data-option-label="${color}"]`).click()
+        cy.get('[id="product-addtocart-button"]').click()
+        cy.get('.page.messages').should('be.visible')
+        cy.get('.page.messages').contains('You added Olivia 1/4 Zip Light Jacket to your shopping cart.')
+        cy.get('.counter-number').contains('1')
+      })
+    }
+    it('Negative case - size is not selected', () => {
       cy.get('[data-option-label="Black"]').click()
       cy.get('[id="product-addtocart-button"]').click()
-      cy.get('.page.messages').should('be.visible')
-      cy.get('.page.messages').contains('You added Olivia 1/4 Zip Light Jacket to your shopping cart.')
-      cy.get('.counter-number').contains('1')
+      cy.get('.page.messages').should('not.be.visible')
+      cy.get('.swatch-attribute.size > div.mage-error').contains('This is a required field.')
     })
-  }
-  for (const color of colors) {
-    it(`select color ${color} and add to the cart`, () => {
-      cy.get(`[data-option-tooltip-value="S"]`).click()
-      cy.get(`[data-option-label="${color}"]`).click()
+    it('Negative case - color is not selected', () => {
+      cy.get('[data-option-tooltip-value="XS"]').click()
       cy.get('[id="product-addtocart-button"]').click()
-      cy.get('.page.messages').should('be.visible')
-      cy.get('.page.messages').contains('You added Olivia 1/4 Zip Light Jacket to your shopping cart.')
-      cy.get('.counter-number').contains('1')
+      cy.get('.page.messages').should('not.be.visible')
+      cy.get('.swatch-attribute.color > div.mage-error').contains('This is a required field.')
     })
-  }
-  for (const quantity of quantityTypes) {
-    it(`add to the cart ${quantity} items`, () => {
+  })
+  describe('Test 2 - Add to the cart different quantity of the product', () => {
+    for (const quantity of quantityTypes) {
+      it(`Test 2 - add to the cart ${quantity} items`, () => {
+        cy.get('[data-option-tooltip-value="XS"]').click()
+        cy.get('[data-option-label="Black"]').click()
+        cy.get('input#qty').clear().type(quantity)
+        cy.get('[id="product-addtocart-button"]').click()
+        cy.get('.page.messages').should('be.visible')
+        cy.get('.page.messages').contains('You added Olivia 1/4 Zip Light Jacket to your shopping cart.')
+        cy.get('.counter-number').contains(quantity)
+      })
+    }
+    it('Negative case - type decimal quantity', () => {
       cy.get('[data-option-tooltip-value="XS"]').click()
       cy.get('[data-option-label="Black"]').click()
-      cy.get('input#qty').clear().type(quantity)
+      cy.get('input#qty').clear().type('2.2')
       cy.get('[id="product-addtocart-button"]').click()
-      cy.get('.page.messages').should('be.visible')
-      cy.get('.page.messages').contains('You added Olivia 1/4 Zip Light Jacket to your shopping cart.')
-      cy.get('.counter-number').contains(quantity)
+      cy.get('.page.messages').should('be.visible').contains('You cannot use decimal quantity for this product.')
     })
-  }
+    it('Negative case - 0 quantity', () => {
+      cy.get('[data-option-tooltip-value="XS"]').click()
+      cy.get('[data-option-label="Black"]').click()
+      cy.get('input#qty').clear().type('0')
+      cy.get('[id="product-addtocart-button"]').click()
+      cy.get('.page.messages').should('not.be.visible')
+      cy.get('[id="qty-error"]').should('be.visible').contains('Please enter a quantity greater than 0.')
+    })
+    it('Negative case - type string instead quantity', () => {
+      cy.get('[data-option-tooltip-value="XS"]').click()
+      cy.get('[data-option-label="Black"]').click()
+      cy.get('input#qty').clear().type('weft')
+      cy.get('[id="product-addtocart-button"]').click()
+      cy.get('.page.messages').should('not.be.visible')
+      cy.get('[id="qty-error"]').should('be.visible').contains('Please enter a valid number in this field.')
+    })
+    it('Negative case - type negative digit instead quantity', () => {
+      cy.get('[data-option-tooltip-value="XS"]').click()
+      cy.get('[data-option-label="Black"]').click()
+      cy.get('input#qty').clear().type('-10')
+      cy.get('[id="product-addtocart-button"]').click()
+      cy.get('.page.messages').should('not.be.visible')
+      cy.get('[id="qty-error"]').contains('Please enter a quantity greater than 0.')
+    })
+    it('Negative case - type negative digit instead quantity', () => {
+      cy.get('[data-option-tooltip-value="XS"]').click()
+      cy.get('[data-option-label="Black"]').click()
+      cy.get('input#qty').clear().type('101')
+      cy.get('[id="product-addtocart-button"]').click()
+      cy.get('.page.messages').should('be.visible').contains('The requested qty is not available')
+    })
+  })
 
-  it(`add to the cart several times`, () => {
+  it(`Test 3 - add to the cart several times`, () => {
     cy.get('[data-option-tooltip-value="XS"]').click()
     cy.get('[data-option-label="Black"]').click()
     cy.get('input#qty').clear().type('2')
@@ -53,58 +107,5 @@ describe('add different size and different count of product to the cart from the
     cy.get('.page.messages').should('be.visible')
     cy.get('.page.messages').contains('You added Olivia 1/4 Zip Light Jacket to your shopping cart.')
     cy.get('.counter-number').contains('12')
-  })
-
-  describe('Negative cases', () => {
-    it('size is not selected', () => {
-      cy.get('[data-option-label="Black"]').click()
-      cy.get('[id="product-addtocart-button"]').click()
-      cy.get('.page.messages').should('not.be.visible')
-      cy.get('.swatch-attribute.size > div.mage-error').contains('This is a required field.')
-    })
-    it('color is not selected', () => {
-      cy.get('[data-option-tooltip-value="XS"]').click()
-      cy.get('[id="product-addtocart-button"]').click()
-      cy.get('.page.messages').should('not.be.visible')
-      cy.get('.swatch-attribute.color > div.mage-error').contains('This is a required field.')
-    })
-    it('type decimal quantity', () => {
-      cy.get('[data-option-tooltip-value="XS"]').click()
-      cy.get('[data-option-label="Black"]').click()
-      cy.get('input#qty').clear().type('2.2')
-      cy.get('[id="product-addtocart-button"]').click()
-      cy.get('.page.messages').should('be.visible').contains('You cannot use decimal quantity for this product.')
-    })
-    it('0 quantity', () => {
-      cy.get('[data-option-tooltip-value="XS"]').click()
-      cy.get('[data-option-label="Black"]').click()
-      cy.get('input#qty').clear().type('0')
-      cy.get('[id="product-addtocart-button"]').click()
-      cy.get('.page.messages').should('not.be.visible')
-      cy.get('[id="qty-error"]').should('be.visible').contains('Please enter a quantity greater than 0.')
-    })
-    it('type string instead quantity', () => {
-      cy.get('[data-option-tooltip-value="XS"]').click()
-      cy.get('[data-option-label="Black"]').click()
-      cy.get('input#qty').clear().type('weft')
-      cy.get('[id="product-addtocart-button"]').click()
-      cy.get('.page.messages').should('not.be.visible')
-      cy.get('[id="qty-error"]').should('be.visible').contains('Please enter a valid number in this field.')
-    })
-    it('type negative digit instead quantity', () => {
-      cy.get('[data-option-tooltip-value="XS"]').click()
-      cy.get('[data-option-label="Black"]').click()
-      cy.get('input#qty').clear().type('-10')
-      cy.get('[id="product-addtocart-button"]').click()
-      cy.get('.page.messages').should('not.be.visible')
-      cy.get('[id="qty-error"]').contains('Please enter a quantity greater than 0.')
-    })
-    it('type negative digit instead quantity', () => {
-      cy.get('[data-option-tooltip-value="XS"]').click()
-      cy.get('[data-option-label="Black"]').click()
-      cy.get('input#qty').clear().type('101')
-      cy.get('[id="product-addtocart-button"]').click()
-      cy.get('.page.messages').should('be.visible').contains('The requested qty is not available')
-    })
   })
 })
